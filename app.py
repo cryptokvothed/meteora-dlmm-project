14431e9b23c7b603394a3f0e3118996c9e97435e
GeekLad <geeklad@geeklad.com>
2025-03-02 15:42:05 -0500

    Updated to use queries instead of views for proper filtering and calculations

--- app.py
+++ app.py
@@ -20,7 +20,6 @@ logging.getLogger('streamlit.runtime').setLevel(logging.WARNING)
 logging.getLogger('streamlit.server').setLevel(logging.WARNING)
 logging.getLogger('watchdog.observers.inotify_buffer').setLevel(logging.WARNING)
 
-# Get the number of updates made
 @sleep_and_retry
 @retry(wait=wait_exponential(multiplier=1.1, min=0.1, max=100))
 @st.cache_data(ttl=60)
@@ -31,13 +30,152 @@ def get_update_count():
   conn.close()
   return update_count
 
-# Get the summary data for the specified # of minutes
 @sleep_and_retry
 @retry(wait=wait_exponential(multiplier=1.1, min=0.1, max=100))
 @st.cache_data(ttl=60, show_spinner="Fetching data...")
 def get_summary_data(num_minutes):
   conn = duckdb.connect(config.DB_FILENAME, read_only=True)
-  query = f"SELECT * FROM v_pair_history where num_minutes={num_minutes}"
+  query = f"""
+    WITH updates AS (
+      SELECT DISTINCT created_at
+      FROM pair_history
+      ORDER BY created_at DESC
+      LIMIT {num_minutes}
+    ), cumulative_stats AS (
+      SELECT h.created_at,
+        p.name,
+        p.pair_address,
+        p.bin_step,
+        p.base_fee_percentage,
+        h.price,
+        h.liquidity,
+        h.fees,
+        count(*) OVER (
+          PARTITION BY p.id
+          ORDER BY created_at
+        ) num_minutes,
+        avg(h.price) OVER (
+          PARTITION BY p.id
+          ORDER BY created_at
+        ) avg_price,
+        sum(h.fees) OVER (
+          PARTITION BY p.id
+          ORDER BY created_at
+        ) cumulative_fees,
+        avg(h.liquidity) OVER (
+          PARTITION BY p.id
+          ORDER BY created_at
+        ) avg_liquidity,
+        round(
+          stddev_samp(h.liquidity) OVER (
+            PARTITION BY p.id
+            ORDER BY created_at
+          ),
+          2
+        ) liquidity_std_dev,
+        round(liquidity_std_dev / avg_liquidity, 2) liquidity_volatility_ratio,
+        CASE
+          WHEN avg_liquidity = 0 THEN 0
+          ELSE 100 * cumulative_fees / (avg_liquidity + liquidity_std_dev)
+        END pct_geek_fees_liquidity,
+        round(
+          60 * 24 * pct_geek_fees_liquidity / num_minutes,
+          2
+        ) pct_geek_fees_liquidity_24h,
+        count(*) FILTER (fees > 0) OVER (
+          PARTITION BY p.id
+          ORDER BY created_at
+        ) num_minutes_with_volume,
+        round(100 * num_minutes_with_volume / num_minutes) pct_minutes_with_volume,
+        coalesce(
+          lag(h.price) OVER (
+            PARTITION BY p.id
+            ORDER BY created_at
+          ) < h.price,
+          false
+        ) tick_up,
+        coalesce(
+          lag(h.price) OVER (
+            PARTITION BY p.id
+            ORDER BY created_at
+          ) > h.price,
+          false
+        ) tick_down,
+        min(h.price) OVER (
+          PARTITION BY p.id
+          ORDER BY created_at
+        ) min_price,
+        max(h.price) OVER (
+          PARTITION BY p.id
+          ORDER BY created_at
+        ) max_price,
+        round(100 * (max_price - min_price) / min_price, 2) pct_price_range,
+        ceil(100 * pct_price_range / p.bin_step) bins_range,
+        ceil(bins_range / 69) num_positions_range,
+        100 * (max_price - h.price) / h.price pct_below_max,
+        ceil(100 * pct_below_max / p.bin_step) bins_below_max,
+        bins_below_max <= 7 near_max,
+        stddev_samp(h.price) OVER (
+          PARTITION BY p.id
+          ORDER BY created_at
+        ) std_dev_price,
+        std_dev_price / avg_price price_volatility_ratio
+      FROM pair_history h
+        JOIN pairs p ON h.pair_id = p.id
+      WHERE NOT p.is_blacklisted
+        AND h.created_at IN (
+          SELECT created_at
+          from updates
+        )
+    )
+    SELECT created_at dttm,
+      name,
+      pair_address,
+      bin_step,
+      base_fee_percentage,
+      price,
+      liquidity,
+      fees,
+      num_minutes,
+      avg_price,
+      cumulative_fees,
+      avg_liquidity,
+      liquidity_std_dev,
+      liquidity_volatility_ratio,
+      pct_geek_fees_liquidity,
+      pct_geek_fees_liquidity_24h,
+      stddev_samp(pct_geek_fees_liquidity_24h) OVER (
+        PARTITION BY pair_address
+        ORDER BY created_at
+      ) std_dev_pct_geek_fees_liquidity_24h,
+      std_dev_pct_geek_fees_liquidity_24h / pct_geek_fees_liquidity_24h pct_geek_fees_liquidity_24h_volatility_ratio,
+      num_minutes_with_volume,
+      pct_minutes_with_volume,
+      sum(tick_up) OVER (
+        PARTITION BY pair_address
+        ORDER BY created_at
+      ) num_tick_up,
+      sum(tick_down) OVER (
+        PARTITION BY pair_address
+        ORDER BY created_at
+      ) num_tick_down,
+      round(
+        100 * num_tick_up / (num_tick_up + num_tick_down)
+      ) pct_tick_up,
+      min_price,
+      max_price,
+      pct_price_range,
+      bins_range,
+      num_positions_range,
+      pct_below_max,
+      bins_below_max,
+      near_max,
+      std_dev_price,
+      price_volatility_ratio
+    FROM cumulative_stats
+    WHERE
+      num_minutes = {num_minutes}
+  """
   summary_data = conn.execute(query).fetchdf()
   conn.close()
   return summary_data
@@ -47,30 +185,258 @@ def get_summary_data(num_minutes):
 @st.cache_data(ttl=60, show_spinner="Fetching pair details...")
 def get_pair_details(pair_address, num_minutes):
   conn = duckdb.connect(config.DB_FILENAME, read_only=True)
-  query = f"SELECT * FROM v_pair_history WHERE pair_address='{pair_address}' and num_minutes<={num_minutes}"
+  query = f"""
+    WITH updates AS (
+      SELECT DISTINCT created_at
+      FROM pair_history
+      ORDER BY created_at DESC
+      LIMIT {num_minutes}
+    ), cumulative_stats AS (
+      SELECT h.created_at,
+        p.name,
+        p.pair_address,
+        p.bin_step,
+        p.base_fee_percentage,
+        h.price,
+        h.liquidity,
+        h.fees,
+        count(*) OVER (
+          PARTITION BY p.id
+          ORDER BY created_at
+        ) num_minutes,
+        avg(h.price) OVER (
+          PARTITION BY p.id
+          ORDER BY created_at
+        ) avg_price,
+        sum(h.fees) OVER (
+          PARTITION BY p.id
+          ORDER BY created_at
+        ) cumulative_fees,
+        avg(h.liquidity) OVER (
+          PARTITION BY p.id
+          ORDER BY created_at
+        ) avg_liquidity,
+        round(
+          stddev_samp(h.liquidity) OVER (
+            PARTITION BY p.id
+            ORDER BY created_at
+          ),
+          2
+        ) liquidity_std_dev,
+        round(liquidity_std_dev / avg_liquidity, 2) liquidity_volatility_ratio,
+        CASE
+          WHEN avg_liquidity = 0 THEN 0
+          ELSE 100 * cumulative_fees / (avg_liquidity + liquidity_std_dev)
+        END pct_geek_fees_liquidity,
+        round(
+          60 * 24 * pct_geek_fees_liquidity / num_minutes,
+          2
+        ) pct_geek_fees_liquidity_24h,
+        count(*) FILTER (fees > 0) OVER (
+          PARTITION BY p.id
+          ORDER BY created_at
+        ) num_minutes_with_volume,
+        round(100 * num_minutes_with_volume / num_minutes) pct_minutes_with_volume,
+        coalesce(
+          lag(h.price) OVER (
+            PARTITION BY p.id
+            ORDER BY created_at
+          ) < h.price,
+          false
+        ) tick_up,
+        coalesce(
+          lag(h.price) OVER (
+            PARTITION BY p.id
+            ORDER BY created_at
+          ) > h.price,
+          false
+        ) tick_down,
+        min(h.price) OVER (
+          PARTITION BY p.id
+          ORDER BY created_at
+        ) min_price,
+        max(h.price) OVER (
+          PARTITION BY p.id
+          ORDER BY created_at
+        ) max_price,
+        round(100 * (max_price - min_price) / min_price, 2) pct_price_range,
+        ceil(100 * pct_price_range / p.bin_step) bins_range,
+        ceil(bins_range / 69) num_positions_range,
+        100 * (max_price - h.price) / h.price pct_below_max,
+        ceil(100 * pct_below_max / p.bin_step) bins_below_max,
+        bins_below_max <= 7 near_max,
+        stddev_samp(h.price) OVER (
+          PARTITION BY p.id
+          ORDER BY created_at
+        ) std_dev_price,
+        std_dev_price / avg_price price_volatility_ratio
+      FROM pair_history h
+        JOIN pairs p ON h.pair_id = p.id
+      WHERE NOT p.is_blacklisted
+        AND h.created_at IN (
+          SELECT created_at
+          from updates
+        )
+    )
+    SELECT created_at dttm,
+      name,
+      pair_address,
+      bin_step,
+      base_fee_percentage,
+      price,
+      liquidity,
+      fees,
+      num_minutes,
+      avg_price,
+      cumulative_fees,
+      avg_liquidity,
+      liquidity_std_dev,
+      liquidity_volatility_ratio,
+      pct_geek_fees_liquidity,
+      pct_geek_fees_liquidity_24h,
+      stddev_samp(pct_geek_fees_liquidity_24h) OVER (
+        PARTITION BY pair_address
+        ORDER BY created_at
+      ) std_dev_pct_geek_fees_liquidity_24h,
+      std_dev_pct_geek_fees_liquidity_24h / pct_geek_fees_liquidity_24h pct_geek_fees_liquidity_24h_volatility_ratio,
+      num_minutes_with_volume,
+      pct_minutes_with_volume,
+      sum(tick_up) OVER (
+        PARTITION BY pair_address
+        ORDER BY created_at
+      ) num_tick_up,
+      sum(tick_down) OVER (
+        PARTITION BY pair_address
+        ORDER BY created_at
+      ) num_tick_down,
+      round(
+        100 * num_tick_up / (num_tick_up + num_tick_down)
+      ) pct_tick_up,
+      min_price,
+      max_price,
+      pct_price_range,
+      bins_range,
+      num_positions_range,
+      pct_below_max,
+      bins_below_max,
+      near_max,
+      std_dev_price,
+      price_volatility_ratio
+    FROM cumulative_stats
+    WHERE
+      pair_address = '{pair_address}'
+  """
   pair_details = conn.execute(query).fetchdf()
   conn.close()
   return pair_details
 
+def get_token_from_list(token_address, token_list):
+  try:
+    return [
+      token_list[x] 
+      for x in range(len(token_list)) 
+      if token_list[x][1] == token_address
+    ][0]
+  except:
+    return None
+
+@sleep_and_retry
+@retry(wait=wait_exponential(multiplier=1.1, min=0.1, max=100))
+@st.cache_data(show_spinner="Fetching pair details...")
+def get_token(pair_address):
+  conn = duckdb.connect(config.DB_FILENAME, read_only=True)
+  query = f"""
+    SELECT 
+      t.symbol, t.mint
+    FROM 
+      pairs p 
+      JOIN tokens t ON p.mint_x_id=t.id OR p.mint_y_id=t.id 
+    WHERE 
+      pair_address='{pair_address}'
+  """
+  results = conn.execute(query).fetchall()
+  mints = [result[1] for result in results]
+  conn.close()
+  base_token_address = [
+    mint for mint in mints 
+    if mint not in [
+      'So11111111111111111111111111111111111111112',
+      'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v',
+      'Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB',
+    ]
+  ]
+  if (len(base_token_address) == 0):
+    if 'So11111111111111111111111111111111111111112' in mints:
+      return get_token_from_list('So11111111111111111111111111111111111111112', results)
+    if 'Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB' in mints:
+      return get_token_from_list('Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB', results)
+    return results[0]
+  return get_token_from_list(base_token_address[0], results)
+
 def display_pair_detail_chart(pair_details):
-  line_chart = alt.Chart(pair_details).mark_line().encode(
-    x=alt.X('dttm:T', title='Time'),
-    y=alt.Y('pct_geek_fees_liquidity_24h:Q', title='Cumulative Geek 24h Fee / TVL')
-  )
+    # Ensure 'dttm' is in datetime format
+    pair_details['dttm'] = pd.to_datetime(pair_details['dttm'])
 
-  bar_chart = alt.Chart(pair_details).mark_bar().encode(
-    x=alt.X('dttm:T', title='Time'),
-    y=alt.Y('fees:Q', title='Fees')
-  )
+    # Melt the DataFrame to combine the metrics into one column
+    melted_pair_details = pair_details.melt(
+        id_vars=['dttm'],
+        value_vars=['pct_geek_fees_liquidity_24h', 'fees'],
+        var_name='Legend',
+        value_name='Value'
+    )
 
-  combined_chart = alt.layer(line_chart, bar_chart).resolve_scale(
-    y='independent'
-  ).properties(
-    width='container',
-    height=400
-  )
+    # Map metric names to more readable labels
+    metric_names = {
+        'pct_geek_fees_liquidity_24h': 'Avg Geek 24h Fee / TVL',
+        'fees': 'Fees'
+    }
+    melted_pair_details['Legend'] = melted_pair_details['Legend'].map(metric_names)  
 
-  st.altair_chart(combined_chart, use_container_width=True)
+    # Create a formatted time column in HH:MM AM/PM format
+    melted_pair_details['Time'] = melted_pair_details['dttm'].dt.strftime('%I:%M %p')
+
+    # Define the color scale to ensure consistent colors
+    color_scale = alt.Scale(
+        domain=['Avg Geek 24h Fee / TVL', 'Fees'],
+        range=['red', 'steelblue']
+    )
+
+    # Create individual charts for each metric
+    line_chart = alt.Chart(
+        melted_pair_details[melted_pair_details['Legend'] == 'Avg Geek 24h Fee / TVL']
+    ).mark_line(strokeWidth=3.5).encode(
+        x=alt.X('dttm:T', title='Time'),
+        y=alt.Y('Value:Q', title='Avg Geek 24h Fee / TVL'),
+        color=alt.Color('Legend:N', scale=color_scale, legend=alt.Legend(title='Legend')),
+        tooltip=[
+            alt.Tooltip('Time:N', title='Time'),
+            alt.Tooltip('Value:Q', title='Avg Geek 24h Fee / TVL')
+        ]
+    )
+
+    bar_chart = alt.Chart(
+        melted_pair_details[melted_pair_details['Legend'] == 'Fees']
+    ).mark_bar(opacity=0.50).encode(
+        x=alt.X('dttm:T', title='Time'),
+        y=alt.Y('Value:Q', title='Fees'),
+        color=alt.Color('Legend:N', scale=color_scale, legend=alt.Legend(title='Legend')),
+        tooltip=[
+            alt.Tooltip('Time:N', title='Time'),
+            alt.Tooltip('Value:Q', title='Fees')
+        ]
+    )
+
+    # Layer the charts and resolve the scales
+    combined_chart = alt.layer(line_chart, bar_chart).resolve_scale(
+        y='independent'
+    ).properties(
+        width='container',
+        height=400,
+        title='Fees and Avg Geek 24h Fee / TVL Over Time'
+    )
+
+    # Display the chart in Streamlit
+    st.altair_chart(combined_chart, use_container_width=True)
 
 def display_num_minutes_selectbox(update_count):
   options=[x for x in [5, 15, 30, 60] if x <= update_count]
@@ -109,11 +475,16 @@ else:
         "type": "greaterThanOrEqual",
         "filter": 1000,
       },
+      "pct_minutes_with_volume": {
+        "filterType": "number",
+        "type": "greaterThan",
+        "filter": 50,
+      },      
       "pct_geek_fees_liquidity_24h": {
         "filterType": "number",
-        "type": "greaterThanOrEqual",
-        "filter": 20,
-      }
+        "type": "greaterThan",
+        "filter": 0,
+      },      
     },
   }
 
@@ -150,6 +521,8 @@ else:
           "sortable": True, 
           "suppressHeaderMenuButton": True, 
           "filter": True,
+          "wrapHeaderText": True,
+          "autoHeaderHeight": True,
         },
         "autoSizeStrategy": {"type": "fitCellContents", "skipHeader": False}, 
         "columnDefs": [
@@ -163,6 +536,7 @@ else:
           },
           { 
             "headerName": "Bin Step", 
+            "maxWidth": 75,
             "field": "bin_step", 
             "type": ["numericColumn", "numberColumnFilter", "customNumericFormat"], 
             "precision": 0,
@@ -174,7 +548,20 @@ else:
           },
           { 
             "headerName": "Base Fee Percentage", 
+            "maxWidth": 100,
             "field": "base_fee_percentage", 
+            "type": ["numericColumn", "numberColumnFilter", "customNumericFormat"], 
+            "precision": 0,
+            "filterParams": {
+              "defaultOption": "greaterThanOrEqual",
+              "buttons": ["apply", "reset"],
+              "closeOnApply": True,
+            }
+          },
+          { 
+            "headerName": "% Minutes w/ Volume", 
+            "maxWidth": 100,
+            "field": "pct_minutes_with_volume", 
             "type": ["numericColumn", "numberColumnFilter", "customNumericFormat"], 
             "precision": 0,
             "filterParams": {
@@ -196,6 +583,7 @@ else:
           },
           { 
             "headerName": "Geek 24h Fee / TVL", 
+            "maxWidth": 120,
             "field": "pct_geek_fees_liquidity_24h", 
             "sort": "desc",
             "type": ["numericColumn", "numberColumnFilter", "customNumericFormat"], 
@@ -215,9 +603,20 @@ else:
     if pair_address == None:
       st.write("Select a row to view details")
     else:
-      name = data[data["pair_address"] == pair_address]["name"].iloc[0]
-      st.write(name)
+      pair = data[data["pair_address"] == pair_address].iloc[0]
+      name = pair["name"]
+      bin_step = pair["bin_step"]
+      base_fee_percentage = pair["base_fee_percentage"]
+      token = get_token(pair_address)
       detail_df = get_pair_details(pair_address, num_minutes)
+      st.markdown(f"""
+          <a href="https://app.meteora.ag/dlmm/{pair_address}" target="_blank">{name} {bin_step} Bin Step, {base_fee_percentage}% Base Fee</a>
+          <a href="https://gmgn.ai/sol/token/{token[1]}" target="_blank">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsrw95DKwTe5yvTSSL0AumcHhLZsiR9SP_1jtO6-_PQltuwbWDWilFY9vv&s" alt="GMGN Link to Token" style="width:20px;height:20px;">
+          </a>
+        """, 
+        unsafe_allow_html=True
+      )
       display_pair_detail_chart(detail_df)
 
   # Show last update time
